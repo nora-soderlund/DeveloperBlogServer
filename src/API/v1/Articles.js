57e4f7ea1123ac814a40cbc7f5d67752b5711831
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 19:40:07 +0100

    Added support for pagination

--- src/API/v1/Articles.js
+++ src/API/v1/Articles.js
@@ -1,31 +1,50 @@
 import Server from "../../Server.js";
 import Database from "../../Database.js";
 
+const limit = 10;
+
 Server.register("GET", "/api/v1/articles", async (request, response) => {
+    const start = Math.max(0, parseInt(request.server.url.searchParams.get("start")));
+
     let query = `
-        SELECT articles.slug, (article_tags.tag = 1) AS featured FROM articles
+        SELECT articles.id, articles.slug, (article_tags.tag = 1) AS featured FROM articles
             INNER JOIN article_tags ON article_tags.article = articles.id
-        WHERE hidden != 1
+        WHERE hidden != 1 ${(start != 0)?(`AND articles.id < ${start}`):("")}
         GROUP BY articles.slug
         ORDER BY
             featured DESC, timestamp DESC
+        LIMIT ${limit}
     `;
 
     const tagsParams = request.server.url.searchParams.get("tags");
 
     if(tagsParams) {
         query = `
-            SELECT articles.slug, (article_tags.tag = 1) AS featured FROM articles
+            SELECT articles.id, articles.slug, (article_tags.tag = 1) AS featured FROM articles
                 INNER JOIN tags ON (${tagsParams.split(',').map((tag) => `tags.slug = ${Database.escape(tag)}`).join(" OR ")})
                 INNER JOIN article_tags ON tag = tags.id
-            WHERE articles.id = article_tags.article
+            WHERE articles.id = article_tags.article ${(start != 0)?(`AND articles.id < ${start}`):("")}
             ORDER BY featured DESC, articles.timestamp DESC
+            LIMIT ${limit}
         `;
     }
 
     const rows = await Database.queryAsync(query);
 
-    return rows.map((row) => {
-        return row.slug;
-    });
+    if(!rows.length) {
+        return {
+            articles: [],
+            paginatable: false
+        }
+    }
+
+    return {
+        end: rows.sort((a, b) => b.id - a.id)[0].id,
+
+        paginatable: rows.length == limit,
+
+        articles: rows.map((row) => {
+            return row.slug;
+        })
+    };
 });
