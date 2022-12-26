49e7ad4036e19bf2307c1177a3245e3ed2ec4fea
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 19:13:11 +0100

    Added support for featured tag

--- src/API/v1/Articles.js
+++ src/API/v1/Articles.js
@@ -2,20 +2,25 @@ import Server from "../../Server.js";
 import Database from "../../Database.js";
 
 Server.register("GET", "/api/v1/articles", async (request, response) => {
-    let query = "SELECT slug FROM articles WHERE hidden != 1 ORDER BY timestamp DESC";
+    let query = `
+        SELECT articles.slug, (article_tags.tag = 1) AS featured FROM articles
+            INNER JOIN article_tags ON article_tags.article = articles.id
+        WHERE hidden != 1
+        GROUP BY articles.slug
+        ORDER BY
+            featured DESC, timestamp DESC
+    `;
 
     const tagsParams = request.server.url.searchParams.get("tags");
 
     if(tagsParams) {
         query = `
-            SELECT articles.slug FROM articles
+            SELECT articles.slug, (article_tags.tag = 1) AS featured FROM articles
                 INNER JOIN tags ON (${tagsParams.split(',').map((tag) => `tags.slug = ${Database.escape(tag)}`).join(" OR ")})
                 INNER JOIN article_tags ON tag = tags.id
             WHERE articles.id = article_tags.article
-            ORDER BY articles.timestamp DESC
+            ORDER BY featured DESC, articles.timestamp DESC
         `;
-
-        console.log(query);
     }
 
     const rows = await Database.queryAsync(query);
