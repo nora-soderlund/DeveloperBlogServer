6a0a8f0cf92086eb08e1ba71ff60f1e72f8fcaf5
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 19:00:22 +0100

    Fixed sorting by tags

--- src/API/v1/Articles.js
+++ src/API/v1/Articles.js
@@ -8,17 +8,14 @@ Server.register("GET", "/api/v1/articles", async (request, response) => {
 
     if(tagsParams) {
         query = `
-            WITH tag_articles AS (
-                SELECT article_tags.id
-                FROM tags
+            SELECT articles.slug FROM articles
+                INNER JOIN tags ON (${tagsParams.split(',').map((tag) => `tags.slug = ${Database.escape(tag)}`).join(" OR ")})
                 INNER JOIN article_tags ON tag = tags.id
-                WHERE (${tagsParams.split(',').map((tag) => `tags.slug = ${Database.escape(tag)}`).join(" OR ")})
-            )
-            SELECT articles.slug
-            FROM articles
-            INNER JOIN tag_articles ON articles.id = tag_articles.id
+            WHERE articles.id = article_tags.article
             ORDER BY articles.timestamp DESC
         `;
+
+        console.log(query);
     }
 
     const rows = await Database.queryAsync(query);
